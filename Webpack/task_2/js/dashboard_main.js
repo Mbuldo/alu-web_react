6827861262106ee936c954a33533c18968df6064
Mbuldo <m.buldo@alustudent.com>
2025-06-11 06:05:42 +0300

    Task done

---
+++ Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,23 @@
+import $ from 'jquery';
+import _ from 'lodash';
+import '../css/main.css';
+import Logo from '../assets/holberton-logo.jpg';
+
+const logo = $('<div id="logo"></div>');
+logo.css('background-image', `url(${Logo})`);
+$('body').prepend(logo);
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count'></p>");
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let count = 0;
+
+function updateCounter() {
+  count += 1;
+  $('#count').text(`${count} clicks on the button`);
+}
+
+$('button').on('click', _.debounce(updateCounter, 300));
