6c988cd844534fceee6c3adaa43e1a4b70095ae8
Mbuldo <m.buldo@alustudent.com>
2025-06-11 06:48:22 +0300

    Task done

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -1,18 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './js/dashboard_main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
-    clean: true, // Clean output folder before each build
+    clean: true
   },
   mode: 'production',
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
@@ -23,12 +24,22 @@ module.exports = {
             options: {
               mozjpeg: { progressive: true },
               optipng: { enabled: false },
-              pngquant: { quality: [0.65, 0.90], speed: 4 },
-              gifsicle: { interlaced: false },
-            },
-          },
-        ],
-      },
-    ],
+              pngquant: { quality: [0.65, 0.9], speed: 4 },
+              gifsicle: { interlaced: false }
+            }
+          }
+        ]
+      }
+    ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html', // use your original HTML file
+      filename: 'index.html'    // output it to public/index.html
+    })
+  ],
+  performance: {
+    maxAssetSize: 512000,
+    maxEntrypointSize: 512000
+  }
 };
