f63b6a4079b53851454d01cea20ee120dbc27f1f
Mbuldo <m.buldo@alustudent.com>
2025-06-11 10:17:50 +0300

    Task done

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -3,21 +3,17 @@ import _ from 'lodash';
 
 let count = 0;
 
-// Function to update the counter
 function updateCounter() {
   count++;
   $('#count').text(`${count} clicks on the button`);
 }
 
-// Add elements to the page when DOM is ready
-$(document).ready(function() {
-  // Add paragraph elements and button in order
+$(document).ready(() => {
   $('body').append('<p>Holberton Dashboard</p>');
   $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button id="start-button">Click here to get started</button>');
-  $('body').append('<p id="count"></p>');
+  $('body').append('<button>Click here to get started</button>');
+  $('body').append("<p id='count'></p>");
   $('body').append('<p>Copyright - Holberton School</p>');
-  
-  // Bind debounced click event to the button
-  $('#start-button').on('click', _.debounce(updateCounter, 500));
+
+  $('button').on('click', _.debounce(updateCounter, 300));
 });
